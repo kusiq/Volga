f77e6a7e4ce0e2273829d076d887dde7057dd08d
Матвей Кусков <73180631+matveikuskov@users.noreply.github.com>
2025-01-31 17:36:09 +0300

    Volga0.3.93 - Optimizing learning

--- data_preparation.py
+++ data_preparation.py
@@ -9,39 +9,33 @@ def validate_data(text):
         raise TypeError("Данные должны быть строкой.")
 
 def load_and_preprocess_data():
-    dataset = load_dataset("wikimedia/wikipedia", "20231101.ru", trust_remote_code=True)
+    # Загрузка Simple Wikipedia
+    dataset = load_dataset("pszemraj/simple_wikipedia")
+    
+    # Разделение данных на train и validation
     train_test_split = dataset['train'].train_test_split(test_size=0.1)
     train_dataset = train_test_split['train']
     validation_dataset = train_test_split['test']
     
-    total_train_articles = len(train_dataset)
-    total_val_articles = len(validation_dataset)
-    
-    sample_size_train = int(total_train_articles * 0.2)  # 20%
-    sample_size_val = int(total_val_articles * 0.2)      # 20%
-    
-    sampled_train_indices = random.sample(range(total_train_articles), sample_size_train)
-    sampled_val_indices = random.sample(range(total_val_articles), sample_size_val)
-    
-    sampled_train_dataset = [train_dataset[i] for i in sampled_train_indices]
-    sampled_val_dataset = [validation_dataset[i] for i in sampled_val_indices]
-    
+    # Сбор текстов статей
     def collect_texts(sampled_dataset):
         texts = []
         for article in sampled_dataset:
-            texts.append(article['text'])
+            texts.append(preprocess_text(article['text']))
         return "\n".join(texts)
     
-    train_text = collect_texts(sampled_train_dataset)
-    val_text = collect_texts(sampled_val_dataset)
+    train_text = collect_texts(train_dataset)
+    val_text = collect_texts(validation_dataset)
     
+    # Предобработка текста
     def preprocess_text(text):
         text = re.sub(r'\s+', ' ', text)
         text = text.strip()
         return text
     
-    train_text = preprocess_text(train_text[:100_000_000])  # 100 млн символов
-    val_text = preprocess_text(val_text[:10_000_000])       # 10 млн символов
+    # Ограничение объёма данных
+    train_text = preprocess_text(train_text[:50_000_000])  # 10 млн символов
+    val_text = preprocess_text(val_text[:5_000_000])      # 1 млн символов
     
     print(f"Total length of train text: {len(train_text)} characters")
     print(f"Total length of validation text: {len(val_text)} characters")
